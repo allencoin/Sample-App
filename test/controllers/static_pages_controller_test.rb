6ded45330325985e5252d630be027452e2163699
Allen Coin <allencoin@gmail.com>
2015-07-20 12:19:05 -0400

    set up bootstrap layout and styles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,28 +9,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    # title test
     assert_select "title", "Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    # title test
     assert_select "title", "Help #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	# title test
   	assert_select "title", "About #{@base_title}"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
-  	#title test
   	assert_select "title", "Contact #{@base_title}"
   end
 end
