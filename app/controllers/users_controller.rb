53e5ffb034ec16b3bc1e646188b566527cbeccc9
Allen Coin <allencoin@gmail.com>
2015-07-25 08:45:22 -0400

    redirect to user detail page upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)		# Not final implementation
   	if @user.save
-  		# Handle a successful save
+      # redirects to user page, same as below:
+      # redirect_to user_url(@user)
+  		redirect_to @user
   	else
   		render 'new'
   	end
