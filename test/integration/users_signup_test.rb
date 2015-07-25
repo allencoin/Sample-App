eaab01f7cbc859587b9931e64d42ed2538fda163
Allen Coin <allencoin@gmail.com>
2015-07-25 16:06:24 -0400

    add check for flash messages in valid signup information integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	  	end
 	  	assert_template 'users/new'
 	  	assert_select 'div#error_explanation'
-	  	assert_select 'div.alert-danger'
+	  	assert_select 'div.field_with_errors'
 	end
 
 	test "valid signup information" do
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 									 password_confirmation: "password" }
 		end
 		assert_template 'users/show'
+		assert_not flash.empty?
 	end
 end
 
