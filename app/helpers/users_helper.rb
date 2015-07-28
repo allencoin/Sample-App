578b0e4f6e26b52579a881b4ba38ae6056d1a77e
Allen Coin <allencoin@gmail.com>
2015-07-28 14:25:06 -0400

    work on rendering profile info in nav bar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,12 @@ module UsersHelper
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
+
+	# Returns tiny gravatar for the given user
+	def tiny_gravatar_for(user, options = { size: 5 })
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
