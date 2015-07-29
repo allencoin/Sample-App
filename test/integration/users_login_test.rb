8428b24d8b512d8e0f301c513edd0d9badca487a
Allen Coin <allencoin@gmail.com>
2015-07-29 11:05:23 -0400

    improve 'remember me' test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -49,7 +49,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not_nil cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
 
   test "login without remembering" do
@@ -58,3 +58,5 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
 end
+
+
