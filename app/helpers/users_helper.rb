60f33516e2b387750e21902ae281c35ce85ed56c
Allen Coin <allencoin@gmail.com>
2015-07-30 19:48:59 -0400

    fix username / gravatar in navbar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
 	# Returns the gravatar for the given user
-	def gravatar_for(user, options = { size: 80 })
+	def gravatar_for(user, options = { size: 200 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
@@ -9,10 +9,8 @@ module UsersHelper
 	end
 
 	# Returns tiny gravatar for the given user
-	def tiny_gravatar_for(user, options = { size: 5 })
-		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		size = options[:size]
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag(gravatar_url, alt: user.name, class: "gravatar")
-	end
+	def tiny_gravatar_for(user)
+    	gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    	"http://gravatar.com/avatar/#{gravatar_id}.png?s=30"
+  	end
 end
